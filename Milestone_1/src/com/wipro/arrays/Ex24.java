23150a9b73f4dd8101c556a51df410ae8921f90f
user <user@DESKTOP-PTDFHVV>
2020-06-12 11:41:19 +0530

    example

---
+++ Milestone_1/src/com/wipro/arrays/Ex24.java
@@ -0,0 +1,16 @@
+package com.wipro.arrays;
+
+import java.util.Arrays;
+
+public class Ex24 {
+public static void main(String[] args) {
+	int n=Integer.parseInt(args[0]);
+	int a[]=new int[n];
+	for(int i=0;i<n;i++) {
+		a[i]=Integer.parseInt(args[i+1]);
+	}
+	Arrays.sort(a);
+	System.out.println("Minimum value is "+a[0]);
+	System.out.println("Maximum value is "+a[n-1]);
+}
+}
