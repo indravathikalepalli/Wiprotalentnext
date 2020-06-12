da06f570f944e6649cd25e04680bb12e793ce3e8
user <user@DESKTOP-PTDFHVV>
2020-06-12 11:22:46 +0530

    example

---
+++ Milestone_1/src/com/wipro/flowcontrolstatement/Ex22.java
@@ -0,0 +1,10 @@
+package com.wipro.flowcontrolstatement;
+
+public class Ex22 {
+public static void main(String[] args) {
+	int n= 2*3*5;
+	for(int i=1;i<=5;i++) {
+		System.out.println(n*i);
+	}
+}
+}
