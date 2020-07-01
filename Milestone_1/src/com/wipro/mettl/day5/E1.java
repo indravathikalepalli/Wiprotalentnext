a62f1f83c1e4913816ec7c64eef94254067d14d3
user <user@DESKTOP-PTDFHVV>
2020-07-01 23:02:35 +0530

    Initial commit

--- Milestone_1/src/com/wipro/mettl/day5/E1.java
+++ Milestone_1/src/com/wipro/mettl/day5/E1.java
@@ -1,5 +1,13 @@
 package com.wipro.mettl.day5;
-
+/*Is N an exact multiple of M?*/ 
 public class E1 {
-
+	if(input1==0 || input2==0){
+		return 3;
+	}
+	else if(input1%input2==0){
+		return 2;
+	}
+	else{
+		return 1;
+	}
 }
