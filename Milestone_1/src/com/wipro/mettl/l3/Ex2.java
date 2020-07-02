3a215ce9f9f89813c524625f3be0a5119c7f8548
user <user@DESKTOP-PTDFHVV>
2020-07-02 22:56:04 +0530

    Initial commit

---
+++ Milestone_1/src/com/wipro/mettl/l3/Ex2.java
@@ -0,0 +1,31 @@
+package com.wipro.mettl.l3;
+/*Find result after alternate add_sub on N*/
+public class Ex2 {
+	int s=input1;
+	int k=1;
+	if(input2==1){
+		while(input1>0){
+		input1-=1;
+		if(k%2!=0){
+			s-=input1;
+		}
+		else{
+			s+=input1;
+		}
+		k++;
+	}
+	}
+	else{
+		while(input1>0){
+		input1-=1;
+		if(k%2!=0){
+			s+=input1;
+		}
+		else{
+			s-=input1;
+		}
+		k++;
+	}
+	}
+	return s;
+}
---
+++ Milestone_1/src/com/wipro/mettl/l3/Ex3.java
@@ -0,0 +1,5 @@
+package com.wipro.mettl.l3;
+
+public class Ex3 {
+
+}
